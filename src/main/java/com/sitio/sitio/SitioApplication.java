ae45701b3773d1b645c9c2bee76fd41e636a7593
valer <valer@PCValeria>
2024-06-09 10:27:20 -0600

    Inicializacion del proyecto

---
+++ src/main/java/com/sitio/sitio/SitioApplication.java
@@ -0,0 +1,13 @@
+package com.sitio.sitio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitio/sitio/SitioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitio.sitio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
